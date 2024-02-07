d08593f3bb27836788681883b433db90fad34a05
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-02-07 14:50:20 -0300

    Learns basic expect() methods

--- sum.js
+++ sum.js
@@ -2,4 +2,11 @@ function sum(a, b) {
   return a + b;
 }
 
+function myFunction(input) {
+  if (typeof input !== "number") {
+    throw new Error("Invalid input!");
+  }
+}
+
 module.exports = sum;
+module.exports = myFunction;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,44 @@
 const sum = require("./sum");
+const myFunction = require("./sum");
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+// Testing primitives:
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
+});
+
+// Testing non-primitives values (inside objects and arrays)
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+
+  expect(data).toEqual({ one: 1, two: 2 });
+});
+
+// Testing falsy values (.toBeFalsy)
+test("null is falsy", () => {
+  const n = null;
+  expect(n).toBeFalsy();
+});
+
+test("zero is falsy", () => {
+  const z = 0;
+  expect(z).toBeFalsy();
+});
+
+// Testing truthy values (.toBeTruthy)
+test("one is truthy", () => {
+  const o = 1;
+  expect(o).toBeTruthy();
+});
+
+// Testing throws (.toThrow())
+test("Throws on invalid input", () => {
+  expect(() => {
+    const invalidInput = "oibebe";
+    myFunction(invalidInput);
+  }).toThrow();
 });
